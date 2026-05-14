e9cad700ed8cc5409ee8b4755388a7f94eab8d56
Davien <davienerwin@gmail.com>
2026-05-14 12:35:30 +0700

    added login page & register page

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/pages/Home';
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
