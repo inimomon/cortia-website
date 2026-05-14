cca7f1f49ff12741457f2898eff89797a649689b
williamkeane112 <williamkeanekeane@gmai.com>
2026-05-14 11:24:20 +0700

    update server.js use router prediction

--- BE/server.js
+++ BE/server.js
@@ -1,11 +1,15 @@
 // import package
 require("dotenv").config();
 const express = require("express");
+const {connectDB, sequelize} = require("./src/config/db");
 // import model
+const predictionModel = require("./src/model/Prediction");
 // import router
-
+const predictionRouter = require("./src/controller/predictionController");
 const app = express();
 
+// use router
+app.use("/api/v1/prediction")
 app.get("/", (req, res) => {
   res.json("hello");
 });
