67c493d021ae711e86e410f5b29cdd169d6e3da6
Davien <davienerwin@gmail.com>
2026-05-14 11:00:47 +0700

    feat: initialize file structure and setup react-router

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <>
-    <div></div>
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
