acbad8886a09f7b78def5a2eefe1112de07764b0
williamkeane112 <williamkeanekeane@gmai.com>
2026-05-14 10:48:30 +0700

    structure be initail

---
+++ BE/src/config/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASS,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+  },
+);
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database Connected");
+  } catch (err) {
+    console.error("DB Error:", err);
+  }
+};
+
+module.exports = { sequelize, connectDB };
