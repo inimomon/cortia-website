1046ece527f917f3ddd9e207afbc22d8d10b0530
inimomon <richiwirjaya@gmail.com>
2026-05-14 11:23:00 +0700

    added navbar ui

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
+
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
